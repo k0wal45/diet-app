0afec9e222e1a9b7dc326633ec0cc36362a5ad01
k0wal45 <d.kowal.com@gmail.com>
2025-08-21 13:51:15 +0200

    products sorted and displayed

--- components/app/products/ProductsList.tsx
+++ components/app/products/ProductsList.tsx
@@ -1,10 +1,16 @@
 "use client";
 import fetchWithCache from "@/lib/fetchWithCache";
+import { Product } from "@/lib/Types";
 import React, { useEffect, useState } from "react";
+
+interface GroupedProduct {
+  category: string;
+  products: Product[];
+}
 
 const ProductsList = () => {
   const [loading, setLoading] = useState(false);
-  const [productsData, setProductsData] = useState();
+  const [productsData, setProductsData] = useState<GroupedProduct[]>([]);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -15,7 +21,23 @@ const ProductsList = () => {
           "products",
           "/api/products/getProducts"
         );
-        setProductsData(data);
+
+        const grouped = data.reduce(
+          (acc: GroupedProduct[], product: Product) => {
+            const category = product.category || "Uncategorized";
+            let group = acc.find((g) => g.category === category);
+            if (!group) {
+              group = { category, products: [] };
+              acc.push(group);
+            }
+            group.products.push(product);
+            return acc;
+          },
+          []
+        );
+
+        setProductsData(grouped);
+
         setLoading(false);
         return;
       } catch (error) {
@@ -31,7 +53,22 @@ const ProductsList = () => {
   return (
     <section className="flex gap-4 flex-col">
       <h1 className="text-xl font-semibold">Products</h1>
-      {loading ? <p>loading...</p> : <div className="">Products here</div>}
+      {loading ? (
+        <p>loading...</p>
+      ) : (
+        <div className="flex flex-wrap">
+          {productsData.map((item: GroupedProduct) => (
+            <div key={item.category} className="flex flex-col gap-8">
+              <h2 className="text-xl font-semibold">{item.category}</h2>
+              <ul className="p-4 flex flex-col">
+                {item.products.map((product: Product) => (
+                  <li key={product.id}>{product.name}</li>
+                ))}
+              </ul>
+            </div>
+          ))}
+        </div>
+      )}
     </section>
   );
 };
